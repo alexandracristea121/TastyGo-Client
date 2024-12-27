8981ff4c97bef121608f992ca550f8c83e6f8fd4
Alexandra Cristea <alexandracristea121@gmail.com>
2024-12-27 15:38:45 +0200

    sign up activity, main activity

---
+++ app/src/main/java/com/examples/licenta_food_ordering/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.examples.licenta_food_ordering
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.licenta_food_ordering.R
+import com.example.licenta_food_ordering.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        var NavController = findNavController(R.id.fragmentContainerView)
+        var bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.setupWithNavController(NavController)
+        binding.imageView5.setOnClickListener{
+            val bottomSheetDialog = Notification_Bottom_Fragment()
+            bottomSheetDialog.show(supportFragmentManager, "Test")
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
