2fb3f33a518db176e3d6b1547d3da17886279921
Alexandra Cristea <alexandracristea121@gmail.com>
2025-01-11 13:47:56 +0200

    UserModel

---
+++ app/src/main/java/com/examples/licenta_food_ordering/model/UserModel.kt
@@ -0,0 +1,10 @@
+package com.examples.licenta_food_ordering.model
+
+data class UserModel(
+
+    val name: String?=null,
+    val email: String?=null,
+    val password: String?=null,
+    val phone: String?=null,
+    val address: String?=null
+)
